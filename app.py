924a9f83c2c32b5c02a1c5cca7843b92ac05fd5e
alotaibighada <alotaibi.ghada0@gmail.com>
2026-01-21 10:41:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 # =====================
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("elo_logo.png", width=140)
+    st.image("elo_logo.png", width=200)
 with col2:
     st.markdown("""
     <h1 style='margin-bottom:0;'>🧮 Math AI</h1>
